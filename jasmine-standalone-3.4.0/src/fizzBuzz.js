c485814ed6e0ee67548a53725b4519076eef00c0
thomasmarano <thomas.marano2@gmail.com>
2019-05-07 11:27:37 +0100

    :poop:  Add passing code for numbers divisible by 3 and 5

--- jasmine-standalone-3.4.0/src/fizzBuzz.js
+++ jasmine-standalone-3.4.0/src/fizzBuzz.js
@@ -4,6 +4,10 @@ Javabuzz.prototype.isDivisibleByThree = function(number) {
     return (number % 3 === 0);
 };
 
-Javabuzz.prototype.isDivisibleByThree = function(number) {
+Javabuzz.prototype.isDivisibleByFive = function(number) {
     return (number % 5 === 0);
+};
+
+Javabuzz.prototype.isDivisibleByFiveAndThree = function(number) {
+    return ((number % 5 === 0) && (number % 3 === 0));
 };
