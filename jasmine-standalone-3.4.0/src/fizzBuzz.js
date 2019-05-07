a3d39a6022c268ac8fbd275e515138edf0d3fc2f
thomasmarano <thomas.marano2@gmail.com>
2019-05-07 11:22:46 +0100

    Revert ":poop:  Add passing code for checking divisibility by 5"
    This reverts commit 5b5b7e98

--- jasmine-standalone-3.4.0/src/fizzBuzz.js
+++ jasmine-standalone-3.4.0/src/fizzBuzz.js
@@ -3,7 +3,3 @@ var Javabuzz = function () {};
 Javabuzz.prototype.isDivisibleByThree = function(number) {
     return (number % 3 === 0);
 };
-
-Javabuzz.prototype.isDivisibleByFive = function(number) {
-    return (number % 5 === 0)
-};
