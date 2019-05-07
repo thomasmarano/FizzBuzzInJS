c68ef6d9020dd878f6e39f2eb7e6a88f370d5d77
thomasmarano <thomas.marano2@gmail.com>
2019-05-07 11:16:34 +0100

    :white_check_mark:  Add tests for checking divisibility by 5

--- jasmine-standalone-3.4.0/spec/fizzBuzzSpec.js
+++ jasmine-standalone-3.4.0/spec/fizzBuzzSpec.js
@@ -1,23 +1,37 @@
 describe('Javabuzz', function(){
     var  javabuzz;
 
+    beforeEach(function() {
+        javabuzz = new Javabuzz();
+    });
+
     describe('know  when a number is', function(){
 
-
         it('divisible by 3', function(){
-            javabuzz = new Javabuzz();
             expect(javabuzz.isDivisibleByThree(3)).toBe(true);
         });
 
+        it('divisible by 5', function() {
+            expect(javabuzz.isDivisibleByFive(5)).toBe(true)
+        });
+
+        // it('divisible by 3 and 5', function() {
+        //
+        // });
     });
 
     describe('know when a number is NOT', function() {
 
         it('divisible by 3', function(){
-            javabuzz = new Javabuzz();
             expect(javabuzz.isDivisibleByThree(1)).toBe(false);
         });
 
-    });
+        it('divisible by 5', function() {
+            expect(javabuzz.isDivisibleByFive(2)).toBe(false)
+        });
 
+        // it('divisible by 3 and 5', function() {
+        //
+        // });
+    });
 });
