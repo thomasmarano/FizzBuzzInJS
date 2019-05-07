58716b9510854d90a55c439bab36f11641ace691
thomasmarano <thomas.marano2@gmail.com>
2019-05-07 12:00:54 +0100

    :recycle:  Introduce new 'divisor method'

--- jasmine-standalone-3.4.0/src/fizzBuzz.js
+++ jasmine-standalone-3.4.0/src/fizzBuzz.js
@@ -1,13 +1,17 @@
 var Javabuzz = function () {};
 
 Javabuzz.prototype.isDivisibleByThree = function(number) {
-    return (number % 3 === 0);
+    return (this._isDivisibleBy(number, 3));
 };
 
 Javabuzz.prototype.isDivisibleByFive = function(number) {
-    return (number % 5 === 0);
+    return (this._isDivisibleBy(number, 5));
 };
 
 Javabuzz.prototype.isDivisibleByFiveAndThree = function(number) {
-    return ((number % 5 === 0) && (number % 3 === 0));
+    return (this._isDivisibleBy(number, 15));
+};
+
+Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
+    return (number % divisor === 0);
 };
