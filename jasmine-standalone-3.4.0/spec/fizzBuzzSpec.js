193cbad1e5d297ab1be4fe45aa285c08d652ea42
thomasmarano <thomas.marano2@gmail.com>
2019-05-07 11:05:52 +0100

    :white_check_mark:  Add additional test for checking divisibility by 3

--- jasmine-standalone-3.4.0/spec/fizzBuzzSpec.js
+++ jasmine-standalone-3.4.0/spec/fizzBuzzSpec.js
@@ -6,7 +6,16 @@ describe('Javabuzz', function(){
 
         it('divisible by 3', function(){
             javabuzz = new Javabuzz();
-            expect(isDivisibleByThree(3).toBe(true))
+            expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+        });
+
+    });
+
+    describe('know when a number is NOT', function() {
+
+        it('divisible by 3', function(){
+            javabuzz = new Javabuzz();
+            expect(javabuzz.isDivisibleByThree(1)).toBe(false);
         });
 
     });
